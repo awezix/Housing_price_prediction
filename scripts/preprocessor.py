59e3b6fac09142c6802514a625268fd2e63395ef
unknown <mdarshansari3426@gmail.com>
2025-03-20 13:24:01 +0530

    modified preprocessor.py

--- scripts/preprocessor.py
+++ scripts/preprocessor.py
@@ -4,11 +4,13 @@ from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
+import pickle
 
 def preprocess_data(df):
+    df.columns = df.columns.str.strip().str.lower()
 
-    numerical_features=df["bhk","area"] 
-    categorical_feature=df["type","region","status","age"]
+    numerical_features=["bhk","area"]
+    categorical_feature=["type","region","status","age"]
 
     num_transformers=Pipeline(
         steps=[
@@ -36,7 +38,10 @@ if __name__=="__main__":
     # ex for testing above code
     df=pd.read_csv("Datasets/cleaned_data.csv")
     df_processed=preprocess_data(df)
+
+    df_transformed=df_processed.fit_transform(df)
     print("preprocessing done")
 
-
-
+    with open("models/preprocessor.pkl","wb") as f:
+        pickle.dump(df_processed,f)
+    print("preprocessor pipeleine saved ad pickle")
